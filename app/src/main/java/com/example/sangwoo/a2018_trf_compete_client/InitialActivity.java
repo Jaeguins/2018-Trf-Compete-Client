8658179b94bc8146ae1218bf2c4c3c06923b1868
Jaeguins <jsu4838@naver.com>
2018-06-28 21:50:53 +0900

    project initiated

---
+++ app/src/main/java/com/example/sangwoo/a2018_trf_compete_client/InitialActivity.java
@@ -0,0 +1,13 @@
+package com.example.sangwoo.a2018_trf_compete_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+    }
+}
