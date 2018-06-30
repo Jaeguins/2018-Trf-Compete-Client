2dad3df0a4db0f13e410e9e886b5651a9bb75396
Jaeguins <jsu4838@naver.com>
2018-06-30 16:21:19 +0900

    tmap registered to manifest translated string add

--- app/src/main/java/com/example/jsu48/a2018_trf_compete_client/Tmapping.java
+++ app/src/main/java/com/example/jsu48/a2018_trf_compete_client/Tmapping.java
@@ -1,14 +1,24 @@
 package com.example.jsu48.a2018_trf_compete_client;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Tmapping extends AppCompatActivity {
-
+    Button cancelButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tmapping);
-
+        cancelButton=findViewById(R.id.cancelButton2);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Tmapping.this,SubDrive.class);
+                startActivity(i);
+            }
+        });
     }
 }
