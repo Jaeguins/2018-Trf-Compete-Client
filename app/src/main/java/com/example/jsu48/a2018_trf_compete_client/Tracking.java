4cab75ea359fc7a4cc043b32b0bef60a525e0d96
Jaeguins <jsu4838@naver.com>
2018-07-04 20:41:33 +0900

    tmpPathFinder add

--- app/src/main/java/com/example/jsu48/a2018_trf_compete_client/Tracking.java
+++ app/src/main/java/com/example/jsu48/a2018_trf_compete_client/Tracking.java
@@ -1,15 +1,27 @@
 package com.example.jsu48.a2018_trf_compete_client;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
 
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapPoint;
+import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapView;
+
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 public class Tracking extends AppCompatActivity {
     TrackTMapView mapView;
     GPSManager gps;
     GPSThread k;
+    TMapPolyLine path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,32 +33,50 @@ public class Tracking extends AppCompatActivity {
         mapView.setZoomLevel(18);
         k = new GPSThread(mapView, gps);
         k.start();
+        Intent i=getIntent();
+        final TMapPoint targetPoint=new TMapPoint(i.getDoubleExtra("latitude",1.0),i.getDoubleExtra("longitude",1.0));
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                TMapPoint now=new TMapPoint(gps.getLatitude(),gps.getLongitude());
+                try {
+                    Tracking.this.path=new TMapData().findPathData(now,targetPoint);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                }
+                mapView.addTMapPolyLine("path",path);
+            }
+        }).start();
     }
     protected void onDestroy() {
         super.onDestroy();
         k.stopping=true;
     }
-}
-class GPSThread extends Thread{
-    TMapView mapView;
-    GPSManager gps;
-    boolean stopping=false;
-    public GPSThread(TMapView mapView,GPSManager gps){
-        this.mapView=mapView;
-        this.gps=gps;
-    }
-    @Override
-    public void run() {
-        while(true) {
-            double lon=gps.getLongitude(),lat=gps.getLatitude();
-            mapView.setCenterPoint(lon,lat);
-            System.out.println("location update to : "+lon+", "+lat);
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+    class GPSThread extends Thread{
+        TMapView mapView;
+        GPSManager gps;
+        boolean stopping=false;
+        public GPSThread(TMapView mapView,GPSManager gps){
+            this.mapView=mapView;
+            this.gps=gps;
+        }
+        @Override
+        public void run() {
+            while(true) {
+                double lon=gps.getLongitude(),lat=gps.getLatitude();
+                mapView.setCenterPoint(lon,lat);
+                System.out.println("location update to : "+lon+", "+lat);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if(stopping) break;
             }
-            if(stopping) break;
         }
     }
 }
