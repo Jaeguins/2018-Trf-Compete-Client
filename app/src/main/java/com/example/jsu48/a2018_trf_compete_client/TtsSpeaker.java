8796a09f8ac9603db3ab86e6bd9aa97f921972a6
Jaeguins <jsu4838@naver.com>
2018-07-17 11:54:14 +0900

    Tts progress started

--- app/src/main/java/com/example/jsu48/a2018_trf_compete_client/TtsSpeaker.java
+++ app/src/main/java/com/example/jsu48/a2018_trf_compete_client/TtsSpeaker.java
@@ -1,4 +1,14 @@
 package com.example.jsu48.a2018_trf_compete_client;
 
-public class TtsSpeaker {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class TtsSpeaker extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
