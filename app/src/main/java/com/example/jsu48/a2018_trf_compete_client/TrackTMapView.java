366fb727aa92b9badbc57bc03ae74250fdd08ae7
Jaeguins <jsu4838@naver.com>
2018-07-18 17:14:23 +0900

    corner detection fixed arrival image add corner detection exception case (can't find next corner til arrival) handled

--- app/src/main/java/com/example/jsu48/a2018_trf_compete_client/TrackTMapView.java
+++ app/src/main/java/com/example/jsu48/a2018_trf_compete_client/TrackTMapView.java
@@ -25,7 +25,7 @@ public class TrackTMapView extends TMapView {
         for(int i=0;i<field.size()-1;i++){
             Vect tmp=Vect.getVect(new Vect(field.get(i)), new Vect(field.get(i+1)));
             Log.d("path optimize",i+" : "+tmp.getSize()+"");
-            if(tmp.getSize()<=2){
+            if(tmp.getSize()<=5){
                 field.remove(i);
                 i-=1;
             }
@@ -81,17 +81,23 @@ public class TrackTMapView extends TMapView {
                 t.setTMapPoint(field.get(i));
                 addMarkerItem("" + i, t);
             }
-
+            int k=0;
+            gpsM.setLocationCallback();
             while (!stopper) {
-                gpsM.setLocationCallback();
-                double dist = 0.0;
-
-                here = getLeftTopPoint();
                 setTrackingMode(true);
+                double dist = 0.0;
+                here =gpsM.getLocation();
+                TMapMarkerItem z = new TMapMarkerItem();
+                z.setCalloutTitle("here");
+                z.setAutoCalloutVisible(true);
+                z.setCanShowCallout(true);
+                z.setTMapPoint(here);
+                addMarkerItem("here"+k,z);
+                k+=1;
                 /*setCompassMode(true);
                 setSightVisible(true);
                 setZoomLevel(18);*/
-                if(!isDestinated){
+                if(!isDestinated||(here.getLongitude()==0&&here.getLatitude()==0)){
                     try {
                         Thread.sleep(5000);
                     } catch (InterruptedException e) {
@@ -114,24 +120,19 @@ public class TrackTMapView extends TMapView {
                 //자신과 가장 가까운 노드 갱신 TODO 최근접 노드 이전 노드들 삭제여부 검토
                 dist += Vect.getDist(here, closest);
                 //자신과 가장 가까운 노드간 거리 더함
-                int i = minInd, dir = -1;
+                int i = minInd,j=i+1,l=j+1, dir = -1;
                 double angle=0.0;
                 Vect in=new Vect(), out = new Vect();
-                for(;i<field.size();i++){
-                    in = Vect.getVect(new Vect(field.get(minInd)), new Vect(field.get(i)));
-                }
-                int j=i;
-                while(j<field.size()) {
-                    for (; j< field.size() ; j++) {
-                        out = Vect.getVect(new Vect(field.get(i)), new Vect(field.get(j)));
-                    }
+                in = Vect.getVect(new Vect(field.get(i)), new Vect(field.get(j)));
+                out=Vect.getVect(new Vect(field.get(j)),new Vect(field.get(l)));
+                while(l<field.size()) {
+
                     dist += in.getSize();
                     angle = Vect.getAngle(in, out);
-                    Log.d("rotation",minInd+" : "+j+" angle : "+angle);
+                    Log.d("nowRotation",i+"~"+j+" -> "+j+"~"+l+" angle : "+angle);
                     if (angle > Math.PI) {
                         angle = 2 * Math.PI - angle;
                     }
-
                     if (angle > Math.PI * 3 / 4 || angle < Math.PI * -3 / 4) {
                         dir = 0;
                         break;
@@ -143,11 +144,19 @@ public class TrackTMapView extends TMapView {
                         break;
                     } else {
                         in=out;
-                        minInd=i;
-                        i=j;
+                        i+=1;
+                        j+=1;
+                        l+=1;
+                        if(l>=field.size()){
+                            dir=3;
+                            break;
+                        }else {
+                            out = Vect.getVect(new Vect(field.get(j)), new Vect(field.get(l)));
+                        }
                         continue;
                     }
                 }
+                Log.i("nowRotation",dir+"");
                 final int rDir = dir;
                 final double rDist = dist;
                 cont.runOnUiThread(new Runnable() {
@@ -155,35 +164,40 @@ public class TrackTMapView extends TMapView {
                     public void run() {
                         switch (rDir) {
                             case 0:
+                                Log.i("nowRotation","uturn");
                                 cont.changeNow(R.drawable.uturn, rDist);
                                 break;
                             case 1:
+                                Log.i("nowRotation","left");
                                 cont.changeNow(R.drawable.left, rDist);
                                 break;
                             case 2:
+                                Log.i("nowRotation","right");
                                 cont.changeNow(R.drawable.right, rDist);
+                                break;
+                            case 3:
+                                Log.i("nowRotation","arrival");
+                                cont.changeNow(R.drawable.arrival,rDist);
                                 break;
                         }
                     }
                 });
                 //이번 코너 탐지/거리 갱신
-                in=out;
-                j=i;
-                while(j<field.size()) {
-                    for (; j< field.size() ; j++) {
-                        out = Vect.getVect(new Vect(field.get(i)), new Vect(field.get(j)));
-                        if (out.x != 0 || out.y != 0) break;
-                    }
+                i+=1;
+                j+=1;
+                l+=1;
+                dist=0;
+                in = Vect.getVect(new Vect(field.get(i)), new Vect(field.get(j)));
+                out=Vect.getVect(new Vect(field.get(j)),new Vect(field.get(l)));
+                while(l<field.size()) {
                     dist += in.getSize();
                     angle = Vect.getAngle(in, out);
-
+                    Log.i("nextRotation",i+"~"+j+" -> "+j+"~"+l+" angle : "+angle);
                     if (angle > Math.PI) {
                         angle = 2 * Math.PI - angle;
                     }
-                    Log.d("rotation",minInd+" : "+j+" angle : "+angle);
                     if (angle > Math.PI * 3 / 4 || angle < Math.PI * -3 / 4) {
                         dir = 0;
-
                         break;
                     } else if (angle > Math.PI / 4) {
                         dir = 1;
@@ -193,25 +207,40 @@ public class TrackTMapView extends TMapView {
                         break;
                     } else {
                         in=out;
-                        minInd=i;
-                        i=j;
+                        i+=1;
+                        j+=1;
+                        l+=1;
+                        if(l>=field.size()){
+                            dir=3;
+                            break;
+                        }else {
+                            out = Vect.getVect(new Vect(field.get(j)), new Vect(field.get(l)));
+                        }
                         continue;
                     }
                 }
                 final int nDir = dir;
                 final double nDist = dist;
+                Log.i("nextRotation",dir+"");
                 cont.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         switch (nDir) {
                             case 0:
+                                Log.i("nextRotation","uTurn");
                                 cont.changeNext(R.drawable.uturn, nDist);
                                 break;
                             case 1:
+                                Log.i("nextRotation","left");
                                 cont.changeNext(R.drawable.left, nDist);
                                 break;
                             case 2:
+                                Log.i("nextRotation","right");
                                 cont.changeNext(R.drawable.right, nDist);
+                                break;
+                            case 3:
+                                Log.i("nextRotation","arrival");
+                                cont.changeNext(R.drawable.arrival,nDist);
                                 break;
                         }
                     }
